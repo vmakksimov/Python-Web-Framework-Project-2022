d113132252e531217c7870e88bc16b9833839303
Viktor <vmakksimov@gmail.com>
2022-12-07 03:03:04 +0200

    changed static

--- Crypto_web/Crypto_web/settings.py
+++ Crypto_web/Crypto_web/settings.py
@@ -131,18 +131,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-#STATIC_URL = 'static/'
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+
+    BASE_DIR / 'staticfiles',
+
+)
+#BASE_DIR_2 = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_URL = '/static/'
 
 #STATICFILES_DIRS = (
-#    BASE_DIR / 'staticfiles',
+#    os.path.join(BASE_DIR_2, 'staticfiles'),
 #)
-BASE_DIR_2 = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR_2, 'staticfiles'),
-)
 
 AUTH_USER_MODEL = 'accounts.CryptoUser'
 
