61d70daa549c6b8847db82c4a70c2e98ecc60878
Viktor <vmakksimov@gmail.com>
2022-12-07 02:51:14 +0200

    Added static files

--- Crypto_web/Crypto_web/settings.py
+++ Crypto_web/Crypto_web/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -131,10 +131,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+#STATIC_URL = 'static/'
+
+#STATICFILES_DIRS = (
+#    BASE_DIR / 'staticfiles',
+#)
+BASE_DIR_2 = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    BASE_DIR / 'staticfiles',
+    os.path.join(BASE_DIR_2, 'staticfiles'),
 )
 
 AUTH_USER_MODEL = 'accounts.CryptoUser'
