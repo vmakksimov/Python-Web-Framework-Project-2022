e1a0de17fd05ce0542b0f2fb29a10af44aff7948
Viktor <vmakksimov@gmail.com>
2022-12-11 19:19:22 +0200

    deleted model

--- Crypto_web/Crypto_web/partners/forms.py
+++ Crypto_web/Crypto_web/partners/forms.py
@@ -1,34 +1,34 @@
 from django import forms
 from Crypto_web.common.helpers import BootstrapFormMixin
 from Crypto_web.helparticle.models import HelpArticle
-from Crypto_web.partners.models import Partners
 
 
-class CreatePartnersForm(BootstrapFormMixin, forms.ModelForm):
-    def __init__(self, user, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.user = user
-        self._init_bootstrap_form_controls()
 
-    def save(self, commit=True):
-        partners = super().save(commit=False)
-        partners.user = self.user
-        if commit:
-            partners.save()
+#class CreatePartnersForm(BootstrapFormMixin, forms.ModelForm):
+    #def __init__(self, user, *args, **kwargs):
+        #super().__init__(*args, **kwargs)
+        #self.user = user
+        #self._init_bootstrap_form_controls()
 
-        return partners
+    #def save(self, commit=True):
+        #partners = super().save(commit=False)
+        #partners.user = self.user
+        #if commit:
+            #partners.save()
 
-    class Meta:
-        model = Partners
-        fields = ('name', 'image',)
+        #return partners
+
+    #class Meta:
+        #m#odel = Partners
+        #fields = ('name', 'image',)
 
 
-class DeletePartnersForm(forms.ModelForm):
+#class DeletePartnersForm(forms.ModelForm):
 
-    def save(self, commit=True):
-        self.instance.delete()
-        return self.instance
+    #def save(self, commit=True):
+        #self.instance.delete()
+        #return self.instance
 
-    class Meta:
-        model = Partners
-        fields = ()
+    #class Meta:
+        #model = Partners
+        #fields = ()
