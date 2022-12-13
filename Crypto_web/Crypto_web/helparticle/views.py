34fd2a752d33ebd2f11683d70974a4c1cb9cd079
Viktor <vmakksimov@gmail.com>
2022-12-14 01:39:00 +0200

    installed plugin heoroku releases retry

--- Crypto_web/Crypto_web/helparticle/views.py
+++ Crypto_web/Crypto_web/helparticle/views.py
@@ -24,7 +24,7 @@ class CreateArticleView(views.CreateView):
 
 class HelpArticleView(views.ListView):
     model = HelpArticle
-    template_name = 'helparticles/help_article.html'
+    template_name = 'news/news.html'
     context_object_name = 'articles'
 
 
